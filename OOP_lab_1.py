9b5cea08623bef8eed3a3e77521280e5f3e62caa
afterkita <90507595+afterkita@users.noreply.github.com>
2023-12-11 21:08:17 +0200

    Лаб. работа 1 (исправленная)

--- OOP_lab_1.py
+++ OOP_lab_1.py
@@ -7,13 +7,15 @@ class Data:
         self.function = function
         self.delta = delta
         self.point = point
-        self.f = lambdify(x, self.function)
 
     def __str__(self):
         return f"f(x) = {self.function} "
 
-
 class Calc(Data):
+    def __init__(self, function, delta, point):
+        super().__init__(function, delta, point)
+        self.f = lambdify(x, self.function)
+
     def Right(self):
         return round(sym.limit((self.f(x+self.delta) - self.f(x)) / self.delta, x, self.point, dir='+'), 5)
 
@@ -27,11 +29,12 @@ class Calc(Data):
         return sym.diff(self.function, x)
 
 
+
 if __name__ == '__main__':
     x = symbols('x')
 
     delta = 0.000000001
-    function = (6*x)**3 + 45
+    function = (6*x)**4 + 45
     point = 2
 
     a = Calc(function, delta, point)
