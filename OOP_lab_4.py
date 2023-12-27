f7b8da1c9eeeac1ae96913e96b2a924a9a537a63
afterkita <90507595+afterkita@users.noreply.github.com>
2023-12-27 10:15:44 +0200

    Добавил Хотелки

--- OOP_lab_4.py
+++ OOP_lab_4.py
@@ -1,62 +1,91 @@
 import time
 
 
-class Button:
-    def __init__(self, ButtonName):
-        self.ButtonName = ButtonName
 
-    def getName(self):
+class Keyboard:
+    def __init__(self, Keyboard_name):
+        self.app = {'Ctrl+Alt+Delete': 'Запуск окна настроек.', 'CTRL+A': 'Выбор всех элементов в документе или окне.', 'Ctrl+~':'Открытие консоли'}
+        self.Keyboard_name = Keyboard_name
+        self.actions = []
+        self.active_app = []
+
+    def set_actions(self, actions):
+        self.actions = actions
+
+    def get_actions(self):
+        return self.actions
+
+    def new_action(self, action_name):
+        self.actions.append(action_name)
+        self.realise_action(action_name)
+
+    def realise_action(self, press_name):
+        if press_name in self.app:
+            self.active_app.append(self.app[str(press_name)])
+        else:
+            self.active_app.append(str(press_name)+' -без действия')
+
+    def undo_last_action(self):
+        if len(self.actions) > 0:
+            print('\nUndo last action:')
+            last_action = self.actions.pop()
+            print(f'Last press - {last_action}')
+            last_active_app = self.active_app.pop()
+            print(f'Last app - {last_active_app}')
+
+        else:
+            print('No recent clicks')
+
+    def print_actions_history(self):
+        print('History:')
+        for action in self.actions:
+            print(action)
+        print('')
+
+    def print_actve_app(self):
+        print('Active app:')
+        for action in self.active_app:
+            print(action)
+        print('')
+
+class Button:
+    def __init__(self, ButtonName, Keyboard = False):
+        self.ButtonName = ButtonName
+        self.Keyboard = Keyboard
+    def get_name(self):
         return self.ButtonName
 
     def setName(self, newButton):
         self.ButtonName = newButton
-
+    def set_keyboard(self,Keyboard):
+        self.Keyboard = Keyboard
     def use_Button(self):
         print(f'Button pressed: {self.ButtonName}')
         time.sleep(0.5)
-        actions.append(self)
+        self.Keyboard.new_action(self.ButtonName)
 
     def reassign_Button(self, newButton, actions):
         print(f'Reassign: {self.ButtonName} ==> {newButton}')
         for action in actions:
-            if action.getName() == self.ButtonName:
+            if action.get_name() == self.ButtonName:
                 action.setName(newButton)
         return actions
 
 
-def undo_last_action(actions):
-    if len(actions) > 0:
-        last_action = actions.pop()
-        print(f'Last press - {last_action.getName()}')
-    else:
-        print('No recent clicks')
-
-
-def printHistory(actions):
-    print('History:')
-    for action in actions:
-        print(action.getName())
-    print('\n')
-
-
 if __name__ == '__main__':
-    actions = []
+    Keyboard1 = Keyboard('Keyboard')
     print(
         "Создать виртуальную клавиатуру с переназначаемыми действиями для клавиш и комбинаций клавиш, с возможностью отката действий назад.")
-    Button1 = Button('Ctrl+Z')
+    Button1 = Button('Ctrl+Z',Keyboard1)
     Button1.use_Button()
-    Button2 = Button('Ctrl+Alt+Delete')
+    Button2 = Button('Ctrl+Alt+Delete',Keyboard1)
     Button2.use_Button()
 
-    print(
-        "\nПродемонстрировать работу клавиатуры сделал Workflow из нажатий различных комбинаций клавиш и откатов назад. Симулировать демонстрацию нажатий клавиш путем вывода значения в консоль и задержкой между нажатиями")
-    printHistory(actions)
+    Keyboard1.print_actions_history()
+    Keyboard1.print_actve_app()
 
-    undo_last_action(actions)
-    printHistory(actions)
-    Button2.use_Button()
-    printHistory(actions)
+    Keyboard1.undo_last_action()
 
-    print("\nПродемонстрировать переназначение клавиши и комбинации клавиш с перезапуком Workflow")
-    actions = Button2.reassign_Button('Ctrl+G', actions)
-    printHistory(actions)
+    Keyboard1.print_actions_history()
+    Keyboard1.print_actve_app()
+
